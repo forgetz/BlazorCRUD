3c024db48e6e5878a36972c52617e8964bcf212d
Nat <creadz@gmail.com>
2020-12-03 18:09:57 +0700

    first init

---
+++ BlazorCRUD/Data/EmployeeService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmployeeService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<EmployeeInfo> GetEmployee()
+        {
+            return _db.employees.ToList();
+        }
+
+        public EmployeeInfo GetEmployeeById(int id)
+        {
+            return _db.employees.Where(s=>s.EmployeeID == id).FirstOrDefault();
+        }
+
+        public bool CreateEmployee(EmployeeInfo employee)
+        {
+            _db.employees.Add(employee);
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateEmployee(EmployeeInfo employee)
+        {
+            _db.employees.Update(employee);
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteEmployee(EmployeeInfo employee)
+        {
+            _db.employees.Remove(employee);
+            _db.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ BlazorCRUD/Service/FileUpload.cs
@@ -0,0 +1,28 @@
+﻿using BlazorInputFile;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Service
+{
+    public class FileUpload
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public FileUpload(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public async Task UploadAsync(IFileListEntry fileEntry)
+        {
+            var filepath = Path.Combine(_environment.ContentRootPath, "Uploads", fileEntry.Name);
+            var ms = new MemoryStream();
+            await fileEntry.Data.CopyToAsync(ms);
+            using (FileStream fs = new FileStream(filepath, FileMode.Create, FileAccess.Write))
+            {
+                ms.WriteTo(fs);
+            }
+        }
+    }
+}
